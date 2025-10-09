190c5763a7ef166dc70ce15ef405cc234912a65f
Alina Anjum <146615548+alina-28@users.noreply.github.com>
2025-10-09 13:46:31 +0530

    Time: 320 ms (81.22%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,20 @@
-# Write your MySQL query statement below
-select name FROM Employee
+-- # Write your MySQL query statement below
+select name from Employee
 where id in (select managerId from Employee group by managerId having count(*)>=5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- select name FROM Employee
+-- where id in (select managerId from Employee group by managerId having count(*)>=5);
